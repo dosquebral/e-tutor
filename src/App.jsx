2a59b919cc03e908a1d24dbd9a55e530efb2a441
DJ <imdarlenequebral@gmail.com>
2025-01-15 17:26:19 +0800

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,37 @@ import NavbarBanner from './components/Navbar/NavbarBanner'
 import Hero from './components/Hero/Hero'
 import NumberCounter from './components/NumberCounter/NumberCounter'
 import WhyChooseUs from './components/WhyChooseUs/WhyChooseUs'
+import Banner from './components/Banner/Banner'
+import Img1 from './assets/banner1.png'
+import Img2 from './assets/banner2.png'
+
+const BannerData = {
+  image: Img1,
+  tag: "CUSTOMIZE WITH YOUR SCHEDULE",
+  title: "Personalized Professional Online Tutor on Your Schedule",
+  subtitle: "Our scheduling system allows you to select based on your free time. Lorem ipsum demo text for template. Keep track of your students class and tutoring schedules, and never miss your lectures. The best online class scheduling system with easy accessibility.Lorem ipsum is a placeholder text commonly used to demonstrate the visual form",
+  link: "#",
+};
+
+const BannerData2 = {
+  image: Img2,
+  tag: "CUSTOMIZE WITH YOUR SCHEDULE",
+  title: "Talented and Qualified Tutors to Serve You for Help",
+  subtitle: "Our scheduling system allows you to select based on your free time. Lorem ipsum demo text for template. Keep track of your students class and tutoring schedules, and never miss your lectures. The best online class scheduling system with easy accessibility.Lorem ipsum is a placeholder text commonly used to demonstrate the visual form",
+  link: "#",
+};
 
 const App = () => {
   return (
-    <main className="overflow-x-hidden">
+    <main>
       <div>
         <Navbar />
         <NavbarBanner />
         <Hero />
         <NumberCounter />
         <WhyChooseUs />
+        <Banner {...BannerData} />
+        <Banner {...BannerData2} reverse={true}/>
       </div>
     </main>
   )
